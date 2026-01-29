ec08f49431bf85fccef142895cf4558400bdc18e
skies-starred <skies.starred@gmail.com>
2026-01-29 18:28:16 +0530

    fix(Render3D): failing on 1.21.11

--- src/main/kotlin/xyz/aerii/nebulune/utils/Render3D.kt
+++ src/main/kotlin/xyz/aerii/nebulune/utils/Render3D.kt
@@ -7,6 +7,6 @@ import java.awt.Color
 
 fun drawTracer(to: Vec3, color: Color, lineWidth: Float = 3f, depthTest: Boolean = false) {
     val camera = client.gameRenderer.mainCamera
-    val from = camera.position.add(Vec3.directionFromRotation(camera.xRot, camera.yRot))
+    val from = camera.position/*? >= 1.21.11 { *//*()*//*? }*/.add(Vec3.directionFromRotation(camera.xRot/*? >= 1.21.11 { *//*()*//*? }*/, camera.yRot/*? >= 1.21.11 { *//*()*//*? }*/))
     Render3D.drawLine(from, to, color, lineWidth, depthTest)
 }
