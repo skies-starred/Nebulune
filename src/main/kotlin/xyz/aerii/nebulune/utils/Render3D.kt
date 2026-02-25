5ebf64b08760d85a88a20457c66d28e5451c63a7
Gaeritag <125013771+Gaeritag@users.noreply.github.com>
2026-02-25 08:09:20 +0100

    feat(WorldScanner)
    * Added WorldScanner
    Added ClientChunkEvent
    Added drawString
    Added CrystalHollowsQuarter
    Added Structure
    Added StructureType
    
    * Removed oopsie
    
    * internal(WorldScanner)
    
    ---------
    
    Co-authored-by: skies-starred <skies.starred@gmail.com>

--- src/main/kotlin/xyz/aerii/nebulune/utils/Render3D.kt
+++ src/main/kotlin/xyz/aerii/nebulune/utils/Render3D.kt
@@ -10,3 +10,31 @@ fun drawTracer(to: Vec3, color: Color, lineWidth: Float = 3f, depthTest: Boolean
     val from = camera.position/*? >= 1.21.11 { *//*()*//*? }*/.add(Vec3.directionFromRotation(camera.xRot/*? >= 1.21.11 { *//*()*//*? }*/, camera.yRot/*? >= 1.21.11 { *//*()*//*? }*/))
     Render3D.drawLine(from, to, color, lineWidth, depthTest)
 }
+
+fun drawString(
+    text: String,
+    pos: Vec3,
+    color: Int = -1,
+    backgroundColor: Int = 0,
+    scale: Float = 1f,
+    depthTest: Boolean = true,
+    shadow: Boolean = true,
+    increase: Boolean = false
+) {
+    var toScale = scale
+    if (increase) {
+        val camPos = client.gameRenderer.mainCamera.position/*? >= 1.21.11 { *//*()*//*? }*/
+        val dist = camPos.distanceTo(pos)
+
+        toScale *= dist.toFloat() / 3f
+    }
+    Render3D.drawString(
+        text,
+        pos,
+        color,
+        backgroundColor,
+        toScale,
+        depthTest,
+        shadow
+    )
+}
