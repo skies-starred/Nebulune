84a32ee688e3039f03a661749e1be9dae1475f76
skies-starred <skies.starred@gmail.com>
2026-02-27 08:01:40 +0000

    feat(WardrobeHelper): improve auto equip

--- src/main/kotlin/xyz/aerii/nebulune/modules/impl/general/WardrobeHelper.kt
+++ src/main/kotlin/xyz/aerii/nebulune/modules/impl/general/WardrobeHelper.kt
@@ -2,8 +2,10 @@
 
 package xyz.aerii.nebulune.modules.impl.general
 
+import net.minecraft.client.KeyMapping
 import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
 import net.minecraft.world.inventory.ClickType
+import net.minecraft.world.item.Items
 import xyz.aerii.athen.annotations.Load
 import xyz.aerii.athen.api.location.LocationAPI
 import xyz.aerii.athen.events.GuiEvent
@@ -11,28 +13,57 @@ import xyz.aerii.athen.events.InputEvent
 import xyz.aerii.athen.events.PacketEvent
 import xyz.aerii.athen.events.core.on
 import xyz.aerii.athen.events.core.runWhen
+import xyz.aerii.athen.handlers.Chronos
 import xyz.aerii.athen.handlers.React.Companion.and
 import xyz.aerii.athen.handlers.Smoothie
+import xyz.aerii.athen.handlers.Smoothie.client
 import xyz.aerii.athen.handlers.Typo.command
 import xyz.aerii.athen.handlers.Typo.stripped
+import xyz.aerii.athen.mixin.accessors.KeyMappingAccessor
 import xyz.aerii.athen.modules.impl.general.WardrobeKeybinds
+import xyz.aerii.athen.utils.render.Render2D.sizedText
 import xyz.aerii.nebulune.events.TickStartEvent
 
 @Load
 object WardrobeHelper {
     val autoClose by WardrobeKeybinds.config.switch("Auto close after use")
-    val autoEquip = WardrobeKeybinds.config.switch("Auto equip").custom("autoEquip")
+    private val autoEquip = WardrobeKeybinds.config.switch("Auto equip").custom("autoEquip")
     private val _unused by WardrobeKeybinds.config.textParagraph("Automatically equips the wardrobe slot without opening the gui. Use at your own risk.")
+    private val moveEquip by WardrobeKeybinds.config.switch("Equip while moving").dependsOn { autoEquip.value }
+    private val equipDelay by WardrobeKeybinds.config.slider("Click delay", 1, 0, 8, "ticks").dependsOn { autoEquip.value }
+    private val closeDelay by WardrobeKeybinds.config.slider("Close delay", 1, 0, 8, "ticks").dependsOn { autoEquip.value }
+    private val delayVariance by WardrobeKeybinds.config.slider("Max delay variety", 1, 0, 5, "ticks").dependsOn { autoEquip.value }
+
+    private val hud = WardrobeKeybinds.config.hud("Display text") {
+        if (it) return@hud sizedText("Equipping §7[§c2§7]")
+        if (!swapping) return@hud null
+        val slot = slot0 ?: return@hud null
+        sizedText("Equipping §7[§c${(slot.idx - 36) + 1}§7]")
+    }
+
+    private val all: List<KeyMapping>
+        get() = listOf(
+            client.options.keyUp,
+            client.options.keyDown,
+            client.options.keyLeft,
+            client.options.keyRight,
+            client.options.keyJump,
+            client.options.keyShift
+        )
 
     private var slot0: WardrobeKeybinds.WardrobeSlot? = null
-    private var swapping = false
-    private var id = -1
+    private var swapping: Boolean = false
+    private var id: Int = -1
+    private var wait: Int = 0
 
     init {
         on<InputEvent.Keyboard.Press> {
+            val key = keyEvent.key
+
+            if (!moveEquip && swapping) for (a in all) if ((a as KeyMappingAccessor).boundKey.value == key) return@on cancel()
             if (swapping) return@on
-            val slot = WardrobeKeybinds.wardrobeSlots.find { it.value == keyEvent.key }
-                ?.takeIf { it.slot?.item?.isEmpty == false } ?: return@on
+
+            val slot = WardrobeKeybinds.wardrobeSlots.find { it.value == key }?.takeIf { it.slot?.item?.isEmpty == false } ?: return@on
 
             slot0 = slot
             swapping = true
@@ -45,13 +76,15 @@ object WardrobeHelper {
         on<PacketEvent.Receive, ClientboundOpenScreenPacket> {
             if (!swapping) return@on
             if ("Wardrobe" !in title.stripped()) return@on
-            val player = Smoothie.client.player ?: return@on
+            val player = client.player ?: return@on
 
             Smoothie.mainThread {
+                if (!moveEquip) for (a in all) a.isDown = false
                 player.containerMenu = type.create(containerId, player.inventory)
             }
 
             id = containerId
+            wait = equipDelay + (0..delayVariance).random()
             it.cancel()
         }.runWhen(autoEquip.state and LocationAPI.isOnSkyBlock)
 
@@ -61,28 +94,38 @@ object WardrobeHelper {
 
         on<TickStartEvent> {
             if (!swapping) return@on
+            if (!WardrobeKeybinds.inMenu) return@on
+            if (wait-- > 0) return@on
 
-            val player = Smoothie.client.player ?: return@on
+            val player = client.player ?: return@on
             val menu = player.containerMenu ?: return@on
             val slot = slot0 ?: return@on
 
             if (menu.containerId != id) return@on
 
-            val mcSlot = menu.slots.getOrNull(slot.idx) ?: return@on
-            if (mcSlot.item.isEmpty) return@on
+            val mcSlot = menu.slots.getOrNull(slot.idx)?.takeIf { it.item?.isEmpty == false } ?: return@on
+            if (mcSlot.item.item == Items.GRAY_DYE) return@on
 
-            if (!slot.equipped) {
-                Smoothie.client.gameMode?.handleInventoryMouseClick(id, slot.idx, 0, ClickType.PICKUP, player)
-            }
+            if (!slot.equipped) client.gameMode?.handleInventoryMouseClick(id, slot.idx, 0, ClickType.PICKUP, player)
 
-            player.closeContainer()
+            close()
             reset()
         }.runWhen(autoEquip.state and LocationAPI.isOnSkyBlock)
+    }
+
+    @JvmStatic
+    fun close() {
+        val player = client.player ?: return
+
+        Chronos.Tick after closeDelay + (0..delayVariance).random() then {
+            player.closeContainer()
+        }
     }
 
     private fun reset() {
         swapping = false
         slot0 = null
         id = -1
+        wait = 0
     }
 }
