86447294ec822cc591d142002d818abcf018933f
skies-starred <skies.starred@gmail.com>
2026-05-13 15:38:16 +0100

    fix(SlayerHighlightMixin): crash

--- src/main/java/xyz/aerii/nebulune/mixin/mixins/athen/SlayerHighlightMixin.java
+++ src/main/java/xyz/aerii/nebulune/mixin/mixins/athen/SlayerHighlightMixin.java
@@ -8,7 +8,7 @@ import xyz.aerii.nebulune.modules.impl.slayer.BossESP;
 
 @Mixin(SlayerHighlight.class)
 public class SlayerHighlightMixin {
-    @ModifyArg(method = "r", at = @At(value = "INVOKE", target = "Lxyz/aerii/athen/utils/render/Render3D;drawBox$default(Lnet/minecraft/world/phys/AABB;Ljava/awt/Color;FZILjava/lang/Object;)V"), index = 4)
+    @ModifyArg(method = "r", at = @At(value = "INVOKE", target = "Lxyz/aerii/athen/api/rendering/level/impl/extensions/impl/BoxExtensionsKt;extractFrameBox$default(Lnet/minecraft/world/phys/AABB;IFZILjava/lang/Object;)V"), index = 4)
     private int nebulune$r(int par5) {
         return BossESP.INSTANCE.getDepth() ? par5 : par5 & ~8;
     }
