68dee4e1cc7fc26bc68b97437ffff0c705f65296
skies-starred <github@starred.foo>
2026-07-14 22:14:37 +0530

    internal: clean up

--- src/main/kotlin/xyz/aerii/nebulune/utils/Render3D.kt
+++ src/main/kotlin/xyz/aerii/nebulune/utils/Render3D.kt
@@ -6,6 +6,6 @@ import xyz.aerii.library.api.client
 
 fun extractTracer(to: Vec3, color: Int, lineWidth: Float = 3f, depthTest: Boolean = false) {
     val camera = client.gameRenderer.mainCamera
-    val from = camera.position/*? >= 1.21.11 { *//*()*//*? }*/.add(Vec3.directionFromRotation(camera.xRot/*? >= 1.21.11 { *//*()*//*? }*/, camera.yRot/*? >= 1.21.11 { *//*()*//*? }*/))
+    val from = camera.position().add(Vec3.directionFromRotation(camera.xRot(), camera.yRot()))
     extractLine(from.toVector3f(), to.toVector3f(), color, lineWidth, depthTest)
 }
