1fef8efa8099219bd4988aa2a6323c615b9fdda3
skies-starred <github@starred.foo>
2026-07-16 13:56:37 +0530

    fix(DaggerSwap)

--- src/main/kotlin/xyz/aerii/nebulune/modules/impl/slayer/DaggerSwap.kt
+++ src/main/kotlin/xyz/aerii/nebulune/modules/impl/slayer/DaggerSwap.kt
@@ -1,13 +1,16 @@
 package xyz.aerii.nebulune.modules.impl.slayer
 
+import net.minecraft.core.component.DataComponents
 import net.minecraft.network.chat.Component
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.component.CustomData
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
 import xyz.aerii.athen.annotations.Load
 import xyz.aerii.athen.annotations.OnlyIn
 import xyz.aerii.athen.api.location.SkyBlockIsland
 import xyz.aerii.athen.config.Category
-import xyz.aerii.athen.ducks.entity.attachedNames
+import xyz.aerii.athen.ducks.entity.EntityDuck.Companion.attachedNames
 import xyz.aerii.athen.events.PlayerEvent
 import xyz.aerii.athen.events.TickEvent
 import xyz.aerii.athen.modules.Module
@@ -15,6 +18,7 @@ import xyz.aerii.library.api.client
 import xyz.aerii.library.utils.stripped
 import xyz.aerii.nebulune.mixin.accessors.InventoryAccessor
 import xyz.aerii.nebulune.utils.rightClick
+import kotlin.jvm.optionals.getOrNull
 
 @Load
 @OnlyIn(islands = [SkyBlockIsland.CRIMSON_ISLE])
@@ -63,7 +67,7 @@ object DaggerSwap : Module(
         val heldStack = inv.getItem(acc.selectedSlot)
         val heldId = heldStack.getData(DataTypes.ID)
         if (heldId != null && heldId in attr.set) {
-            if (heldId !in attr.set) rightClick()
+            if (heldStack.fn() != attr.mode) rightClick()
             swap = null
             return
         }
@@ -79,11 +83,15 @@ object DaggerSwap : Module(
         swap = null
     }
 
-    private enum class Attunements(val str: String, val set: Set<String>) {
-        Ashen("ASHEN ♨", setOf("HEARTFIRE_DAGGER", "BURSTFIRE_DAGGER", "FIREDUST_DAGGER")),
-        Auric("AURIC ♨", setOf("HEARTFIRE_DAGGER", "BURSTFIRE_DAGGER", "FIREDUST_DAGGER")),
-        Crystal("CRYSTAL ♨", setOf("HEARTMAW_DAGGER", "BURSTMAW_DAGGER", "MAWDUST_DAGGER")),
-        Spirit("SPIRIT ♨", setOf("HEARTMAW_DAGGER", "BURSTMAW_DAGGER", "MAWDUST_DAGGER"));
+    private fun ItemStack.fn(): Int {
+        return getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getInt("td_attune_mode").getOrNull() ?: -1
+    }
+
+    private enum class Attunements(val str: String, val set: Set<String>, val mode: Int) {
+        Ashen("ASHEN ♨", setOf("HEARTFIRE_DAGGER", "BURSTFIRE_DAGGER", "FIREDUST_DAGGER"), 0),
+        Auric("AURIC ♨", setOf("HEARTFIRE_DAGGER", "BURSTFIRE_DAGGER", "FIREDUST_DAGGER"), 1),
+        Crystal("CRYSTAL ♨", setOf("HEARTMAW_DAGGER", "BURSTMAW_DAGGER", "MAWDUST_DAGGER"), 2),
+        Spirit("SPIRIT ♨", setOf("HEARTMAW_DAGGER", "BURSTMAW_DAGGER", "MAWDUST_DAGGER"), 3);
 
         companion object {
             fun get(a: String) = entries.firstOrNull { it.str in a }
